bb298188e931efaa187060ed789d10dba61505c6
Nela <klonczyk@gmail.com>
2019-08-22 15:43:44 +0200

    initial add

---
+++ who_are_you.py
@@ -0,0 +1,13 @@
+name = ''
+while name != 'Lil':
+    print ('Who are you?')
+    name = input()
+    if name == ('Lil'):
+        print ('Hi Lil')
+
+passwd = ''
+while passwd != 'jellyfish':
+    print('Enter your fish password please')
+    passwd = input()
+    if passwd == 'jellyfish':
+        print('Hello, enjoy!')
