b39117d3f2aed1ef7e763e7c159e4e642009adad
Nela <klonczyk@gmail.com>
2019-08-26 21:13:29 +0200

    First commit

---
+++ list_modyfing.py
@@ -0,0 +1,16 @@
+"""Comma Code."""
+
+y = ['sleep', 'eat', 'smile', 'dance']
+
+def mody_list(x):
+    """Take a list and print as a string with commas."""
+    i = 0
+    new = ''
+    while i < len(x)-2:
+        new = (new + str(x[i]) + ', ')
+        i = i + 1
+    new = (new + str(x[-2]) + ' and ' + str(x[-1]))
+    return new
+
+
+print(mody_list(y))
